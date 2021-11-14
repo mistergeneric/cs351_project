af3874c3c779a1a03834166b5dc0e8fc11757310
Jordan Thomson <jordan.thomson.2019@uni.strath.ac.uk>
2021-11-14 21:43:51 +0000

    removal of online/offline listener - not required as it's advising everyone regardless of room or friend status.

--- Server/src/client/UserStatusListener.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import java.io.IOException;
-
-public interface UserStatusListener {
-
-    public void online(String login) throws IOException;
-    public void offline(String login);
-}
