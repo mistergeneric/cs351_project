67830c0412ac197b4c874fbf397db8b13fd01b96
Jordan Thomson <jordan.thomson.2019@uni.strath.ac.uk>
2021-11-16 18:26:00 +0000

    accessible users

--- Server/src/ChatRoom.java
+++ Server/src/ChatRoom.java
@@ -1,16 +1,17 @@
-package chat;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+
 public class ChatRoom {
     private String chatRoomName;
     private HashSet<String> currentUsers;
+    private HashSet<ServerWorker> users;
 
     public ChatRoom(String chatRoomName, HashSet<String> currentUsers) {
         this.chatRoomName = chatRoomName;
         this.currentUsers = currentUsers;
+        users = new HashSet<>();
     }
 
     public String getChatRoomName() {
@@ -27,5 +28,17 @@ public class ChatRoom {
 
     public void setCurrentUsers(HashSet<String> currentUsers) {
         this.currentUsers = currentUsers;
+    }
+
+    public void addUser(ServerWorker worker) {
+        users.add(worker);
+    }
+
+    public void removeUser(ServerWorker worker) {
+        users.remove(worker);
+    }
+
+    public HashSet<ServerWorker> getUsers() {
+        return users;
     }
 }
